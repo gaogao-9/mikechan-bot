65166449a87e180e6909ce1f6402ff01d8aa4f40
がお <gaotuki@gmail.com>
2016-08-21 19:06:08 +0900

    適当な鯖を立てた

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,2 @@
-"use strict";process.env.NODE_PATH=__dirname+"/lib";require('module')._initPaths();if(process.env.NODE_ENV!=="production"){try{require("./slackToken");}catch(err){}}require("babel-polyfill");require("./main");
+"use strict";process.env.NODE_PATH=__dirname+"/lib";require('module')._initPaths();if(process.env.NODE_ENV!=="production"){try{require("./slackToken");}catch(err){}}else{// herokuはサーバー立てないと再起動するらしい(闇)
+require("http").createServer((req,res)=>{res.writeHead(200,"OK",{"Content-Type":"text/plain; charset=UTF-8"});res.end("わたし、岬明乃！");}).listen(process.env.PORT);}require("babel-polyfill");require("./main");
--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ if(process.env.NODE_ENV !== "production"){
 	}
 	catch(err){}
 }
+else{
+	// herokuはサーバー立てないと再起動するらしい(闇)
+	require("http").createServer((req, res)=> {
+		res.writeHead(200, "OK", {
+			"Content-Type": "text/plain; charset=UTF-8",
+		});
+		res.end("わたし、岬明乃！");
+	}).listen(process.env.PORT);
+}
 
 require("babel-polyfill");
 require("./main");
